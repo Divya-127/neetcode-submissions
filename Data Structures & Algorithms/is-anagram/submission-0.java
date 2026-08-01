02d740270c87acefcff85b9054dd60bedabe32a9
Divya Shah <63257999+Divya-127@users.noreply.github.com>
2026-08-01 22:31:22 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+        return false;
+        HashMap<Character,Integer> hm1 = new HashMap<Character,Integer>();
+        HashMap<Character,Integer> hm2 = new HashMap<Character,Integer>();
+        for(int i=0;i<s.length();i++)
+        {
+            hm1.merge(s.charAt(i),1,Integer::sum);
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            hm2.merge(t.charAt(i),1,Integer::sum);
+        }
+        for (Character key : hm2.keySet()) {  
+            if(!Objects.equals(hm1.get(key), hm2.get(key)))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
